bfc45b85bb7c99b017f4e0759ef67fdaad330125
LunaBestPone <saltyprinx@gmail.com>
2021-12-05 17:36:50 -0500

    checkpoint2

--- stats_collector.cpp
+++ stats_collector.cpp
@@ -1,3 +1,4 @@
+#include "llvm-c/Core.h"
 #include "llvm/Pass.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/IR/Function.h"
@@ -9,6 +10,7 @@
 
 #include <unordered_set>
 #include <vector>
+#include <algorithm>    // std::sort
 using namespace llvm;
 
 namespace {
@@ -24,21 +26,63 @@ struct stats_collector : public FunctionPass {
 		StringRef main_name_strRef(main_name);
 		if (F.getName() == main_name_strRef){
 			LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
-			auto Traces = traceFormation(&LI);
+			auto Traces = traceFormation(&LI, F);
 		}
 		return false;
 	}
 
-	std::vector<std::vector<BasicBlock*>> traceFormation(LoopInfo* LI) {
+	std::vector<std::vector<BasicBlock*>> traceFormation(LoopInfo* LI, Function& F) {
 		std::vector<std::vector<BasicBlock*>> res;
-		std::unordered_set<BasicBlock*> seen;
+		std::unordered_set<BasicBlock*> seen_in_trace;
 		auto loopVec_PreOrd = LI->getLoopsInPreorder();
+
+		sort(loopVec_PreOrd.begin(), loopVec_PreOrd.end(), LoopDepthGt);
+		for (auto* loop : loopVec_PreOrd) {
+			// create BFS Traversal of basic blocks in this loop
+			std::unordered_set<BasicBlock*> seen;
+			std::vector<BasicBlock*> BFSorder;
+			std::vector<BasicBlock*> queue;
+			queue.insert(queue.begin(), loop->getHeader());
+			seen.insert(loop->getHeader());
+			BFSorder.push_back(loop->getHeader());
+			while (!queue.empty()) {
+				// left side queue entry, right side queue exit
+				BasicBlock* cur_node = queue.back();
+				for (BasicBlock* child : successors(cur_node)) {
+					if (seen.find(child) == seen.end() && loop->contains(child)) {
+						seen.insert(child);
+						queue.insert(queue.begin(), child);
+						BFSorder.push_back(child);
+					}
+				}
+				queue.pop_back();
+			}
+			// process blocks in loops
+			for (BasicBlock* cur_seed : BFSorder) {
+				if (seen_in_trace.find(cur_seed) == seen_in_trace.end()) {
+					auto cur_trace = growTrace(cur_seed, seen_in_trace);
+				}
+			}
+		}
+		// create BFS Traversal of basic blocks in the function
+		std::unordered_set<BasicBlock*> seen;
+		std::vector<BasicBlock*> BFSorder;
+		std::vector<BasicBlock*> queue;
+		for (BasicBlock& BB: F) {
+			
+		}
+
+
 		return res;
 	}
 
 	std::vector<BasicBlock*> growTrace(BasicBlock* seed, std::unordered_set<BasicBlock*> seen) {
 		std::vector<BasicBlock*> res;
 		return res;
+	}
+
+	static bool LoopDepthGt(Loop* i, Loop* j) {
+		return i->getLoopDepth() > j->getLoopDepth();
 	}
 
 }; // end of struct Hell
