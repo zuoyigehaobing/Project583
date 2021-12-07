4949db2d073966c2734cfcc4d025b64c7ff8243e
LunaBestPone <saltyprinx@gmail.com>
2021-12-07 00:09:56 -0500

    checkpoint hazard heuristic

--- stats_collector.cpp
+++ stats_collector.cpp
@@ -1,4 +1,3 @@
-#include "llvm-c/Core.h"
 #include "llvm/Pass.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/IR/Function.h"
@@ -7,10 +6,13 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/Format.h"
 #include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/PostDominators.h"
 
 #include <unordered_set>
 #include <vector>
 #include <algorithm>    // std::sort
+#include <map>
+
 using namespace llvm;
 
 namespace {
@@ -20,22 +22,42 @@ struct stats_collector : public FunctionPass {
 
 	void getAnalysisUsage(AnalysisUsage &AU) const {
 		AU.addRequired<LoopInfoWrapperPass>();
+		AU.addRequired<PostDominatorTreeWrapperPass>();
 	}
 	bool runOnFunction(Function &F) override {
-		const char* main_name = "main";
-		StringRef main_name_strRef(main_name);
-		if (F.getName() == main_name_strRef){
-			LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
-			auto Traces = traceFormation(&LI, F);
-		}
-		return false;
+		LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
+		PostDominatorTree& PDT = getAnalysis<PostDominatorTreeWrapperPass>().getPostDomTree();
+		auto Traces = traceFormation(&LI, &PDT, F);
 	}
 
-	std::vector<std::vector<BasicBlock*>> traceFormation(LoopInfo* LI, Function& F) {
+	std::vector<std::vector<BasicBlock*>> traceFormation(LoopInfo* LI, PostDominatorTree* PDT, Function& F) {
+		// res is the 2d vector of traces
 		std::vector<std::vector<BasicBlock*>> res;
 		std::unordered_set<BasicBlock*> seen_in_trace;
-		auto loopVec_PreOrd = LI->getLoopsInPreorder();
 
+		// before growing trace with seed, identify basic blocks with hazardous instructions
+		std::map<BasicBlock*, bool> contains_hazard;
+		std::unordered_set<unsigned> hazardous_opcodes = {
+			Instruction::CallBr,
+			Instruction::Invoke,
+			Instruction::Ret,
+			Instruction::IndirectBr,
+		}
+		for (BasicBlock& BB : F) {
+			for (Instruction& I : BB) {
+				unsigned cur_opcode = I.getOpcode();
+				if (hazardous_opcodes.count(cur_opcode)) {
+					contains_hazard[&BB] = true;
+				}
+				else if (cur_opcode == Instruction::Store) {
+					//dyn_cast pointer argument to Instruction, check if it is getelementptr
+				}
+			}
+		}
+
+
+		// grow blocks in loops
+		auto loopVec_PreOrd = LI->getLoopsInPreorder();
 		sort(loopVec_PreOrd.begin(), loopVec_PreOrd.end(), LoopDepthGt);
 		for (auto* loop : loopVec_PreOrd) {
 			// create BFS Traversal of basic blocks in this loop
@@ -61,23 +83,51 @@ struct stats_collector : public FunctionPass {
 			for (BasicBlock* cur_seed : BFSorder) {
 				if (seen_in_trace.find(cur_seed) == seen_in_trace.end()) {
 					auto cur_trace = growTrace(cur_seed, seen_in_trace);
+					res.insert(res.begin(),cur_trace);
 				}
 			}
 		}
+
+		// grow remaining blocks in the function
 		// create BFS Traversal of basic blocks in the function
 		std::unordered_set<BasicBlock*> seen;
 		std::vector<BasicBlock*> BFSorder;
 		std::vector<BasicBlock*> queue;
-		for (BasicBlock& BB: F) {
-			
+		BasicBlock& entryBlock = F.getEntryBlock();
+		queue.insert(queue.begin(), &entryBlock);
+		seen.insert(&entryBlock);
+		BFSorder.push_back(&entryBlock);
+
+		// BFS starts
+		while (!queue.empty()) {
+			// left side queue entry, right side queue exit
+			BasicBlock* cur_node = queue.back();
+			for (BasicBlock* child : successors(cur_node)) {
+				if (seen.find(child) == seen.end()) {
+					seen.insert(child);
+					queue.insert(queue.begin(), child);
+					if (seen_in_trace.find(child) == seen_in_trace.end()) {
+						BFSorder.push_back(child);
+					}
+				}
+			}
+
+			// pop from the queue
+			queue.pop_back();
 		}
 
-
+		for (BasicBlock* cur_seed : BFSorder) {
+			if (seen_in_trace.find(cur_seed) == seen_in_trace.end()) {
+				auto cur_trace = growTrace(cur_seed, seen_in_trace);
+				// add cur trace to res
+				res.push_back(cur_trace);
+			}
+		}
 		return res;
 	}
 
 	std::vector<BasicBlock*> growTrace(BasicBlock* seed, std::unordered_set<BasicBlock*> seen) {
-		std::vector<BasicBlock*> res;
+		 std::vector<BasicBlock*> res;
 		return res;
 	}
 
